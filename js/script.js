e7872238e2ed7857a21a5ea86cbfb1f91a38f2f0
Karen He <hainechoko@gmail.com>
2018-10-19 01:25:17 +0000

    started the project. added some js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,25 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+var first
+var second
+var third
+var fourth
 
 
 // DOCUMENT READY FUNCTION BELOW
+$(document).ready(function(){
+    first=Math.floor(Math.random()*10);
+    
+});
+
+
+$("#verify").click(function () {
+    var numOne = $("#number1").val();
+    var numTwo = $("#number2").val();
+    var numThree = $("#number3").val();
+    var numFour =  $("#number4").val();
+    $("#userChoice").text(numOne + numTwo + numThree + numFour);
+});
+    
 
