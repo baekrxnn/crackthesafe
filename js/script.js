9eab08eaddfeb9d6ed212956dd957782e6bf050a
Karen He <hainechoko@gmail.com>
2018-10-26 01:23:28 +0000

    changed var to let, added conditionals, changed randomNumber variables to strings

--- js/script.js
+++ js/script.js
@@ -4,25 +4,57 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-var first
-var second
-var third
-var fourth
+let first;
+let second;
+let third;
+let fourth;
 
+let guessOne="";
+let guessTwo="";
+let guessThree="";
+let guessFour="";
+
+let numbersCorrect=0;
+let numbersInPlace=0;
 
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
     first=Math.floor(Math.random()*10);
+    let randomNumber1= first.toString();
+    
+    second=Math.floor(Math.random()*10);
+    let randomNumber2= second.toString();
+    
+    third=Math.floor(Math.random()*10);
+    let randomNumber3= third.toString();
+    
+    fourth=Math.floor(Math.random()*10);
+    let randomNumber4= fourth.toString();
+
+
+    $("#verify").click(function () {
+        guessOne = $("#number1").val();
+        guessTwo = $("#number2").val();
+        guessThree = $("#number3").val();
+        guessFour =  $("#number4").val();
+        $("#userChoice").text(guessOne + guessTwo + guessThree + guessFour);
+        
+        if (guessOne===randomNumber1) {
+            
+        };
+    
+        if (guessTwo===randomNumber2) {
+            
+        };
+    
+        if (guessThree===randomNumber3) {
+            
+        };
+    
+        if (guessFour===randomNumber4) {
+            
+        };
+    
+    });
     
 });
-
-
-$("#verify").click(function () {
-    var numOne = $("#number1").val();
-    var numTwo = $("#number2").val();
-    var numThree = $("#number3").val();
-    var numFour =  $("#number4").val();
-    $("#userChoice").text(numOne + numTwo + numThree + numFour);
-});
-    
-
